9e2f8195af69d249ab7b207a927af8a2f0faac54
hulichao <jsu_hlc@163.com>
2020-12-01 12:11:35 +0800

    leet26 注释

---
+++ src/com/hoult/doublpoint/Leet26.java
@@ -0,0 +1,15 @@
+package com.hoult.doublpoint;
+
+public class Leet26 {
+//    public int removeDuplicates(int[] nums) {
+//        if (nums == null)
+//            return 0;
+//
+//        int i = nums.length - 1,j = i - 1;
+//        while (j >= 0) {
+//            if (nums[i] == nums[j]) {
+////                nums[j] = null;
+//            }
+//        }
+//    }
+}
